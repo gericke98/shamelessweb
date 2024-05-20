2e8c45410cce7911f181e9c188c20df08673a897
gericke98 <sgerickee@gmail.com>
2024-05-20 23:04:23 +0200

    hero changes

--- app/(main)/hero.tsx
+++ app/(main)/hero.tsx
@@ -5,12 +5,12 @@ export const Hero = () => {
   return (
     <div className="w-full h-full">
       <Banner text="PENINSULAR FREE SHIPPING" variant="dark" repeat={5} />
-      <div className="h-screen w-full relative">
+      <div className="h-screen w-full relative flex justify-center items-center">
         <Image
           src="/heropc.jpg"
           alt="Hero"
           fill
-          style={{ objectFit: "cover", objectPosition: "15% 12%" }}
+          className="object-cover"
           priority
         />
       </div>
