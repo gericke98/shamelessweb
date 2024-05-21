4b552f3e8f26aa800c305ae0278ecfc13961928a
gericke98 <sgerickee@gmail.com>
2024-05-21 21:07:35 +0200

    carousel

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -206,7 +206,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 rounded-full",
+        "absolute lg:h-8 lg:w-8 h-4 w-4 rounded-full",
         orientation === "horizontal"
           ? "lg:left-12 left-1 top-1/2 -translate-y-1/2"
           : "top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -236,7 +236,7 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full black",
+        "absolute lg:h-8 lg:w-8 h-4 w-4 rounded-full black",
         orientation === "horizontal"
           ? "lg:right-12 right-1 top-1/2 -translate-y-1/2"
           : "bottom-12 left-1/2 -translate-x-1/2 rotate-90",
